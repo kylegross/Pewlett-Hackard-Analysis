6889e03e307c346aa1f95fa91f390214e8b81790
Gross <kgross@asg-architects.com>
2020-08-27 21:45:49 -0400

    Adding final employee files.

--- Data/emp_count.sql
+++ Data/emp_count.sql
@@ -133,3 +133,45 @@ ON (e.emp_no = de.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
      AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 	 AND (de.to_date = '9999-01-01');
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp AS ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO sales_retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM sales_retirement_info;
+-- Joining retirement_info and dept_emp tables
+SELECT sri.emp_no,
+    sri.first_name,
+sri.last_name,
+    de.to_date,
+	de.dept_no
+FROM sales_retirement_info as sri
+LEFT JOIN dept_emp as de
+ON sri.emp_no = de.emp_no
+WHERE (dept_no = 'd007');
